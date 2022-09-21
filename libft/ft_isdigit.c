680497fcb4817658ad8002aea77f0d22020993a5
Sergio Diaz Ruiz <sdiaz-ru@student.42malaga.com>
2022-09-21 16:09:25 +0200

    Creada ft_isdigit() y anadida en libft.h y main.c, tambien implementado el main.c para usar argumentos

--- libft/ft_isdigit.c
+++ libft/ft_isdigit.c
@@ -1,22 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sdiaz-ru <sdiaz-ru@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/21 11:49:45 by sdiaz-ru          #+#    #+#             */
-/*   Updated: 2022/09/21 16:04:02 by sdiaz-ru         ###   ########.fr       */
+/*   Created: 2022/09/21 16:02:14 by sdiaz-ru          #+#    #+#             */
+/*   Updated: 2022/09/21 16:06:13 by sdiaz-ru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-#include "ft_isalpha.c"
-#include "ft_isdigit.c"
-
-int	ft_isalpha(int c);
-int ft_isdigit(int c);
-
-#endif
+int ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (2048);
+	return (0);
+}
--- libft/main.c
+++ libft/main.c
@@ -6,7 +6,7 @@
 /*   By: sdiaz-ru <sdiaz-ru@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 11:48:44 by sdiaz-ru          #+#    #+#             */
-/*   Updated: 2022/09/21 13:03:12 by sdiaz-ru         ###   ########.fr       */
+/*   Updated: 2022/09/21 16:06:23 by sdiaz-ru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,41 @@
 #include <ctype.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 
 int	main(int argc, char **argv)
 {
-	int	i;
-
-	(void)argc;
-	(void)argv;
-	i = 0;
-	while (i < 255)
+	if (!strcmp(argv[1], "isalpha"))
 	{
-		if (isalpha(i) == ft_isalpha(i))
+		int	i;
+
+		(void)argc;
+		(void)argv;
+		i = -5;
+		while (i < 300)
 		{
-			printf("%d \n", i);
-			printf("Real: %d \n", isalpha(i));
-			printf(" Mia: %d \n", ft_isalpha(i));
+			if (isalpha(i) == ft_isalpha(i))
+			{
+				printf("%d \n", i);
+				printf("Real: %d \n", isalpha(i));
+				printf(" Mia: %d \n", ft_isalpha(i));
+			}
+			i++;
 		}
-		i++;
+	}
+	if (!strcmp(argv[1], "isdigit"))
+	{
+		int	i;
+		i = -5;
+		while (i < 300)
+		{
+			if (isdigit(i) == ft_isdigit(i))
+			{
+				printf("%d \n", i);
+				printf("Real: %d \n", isdigit(i));
+				printf(" Mia: %d \n", ft_isdigit(i));
+			}
+			i++;
+		}
 	}
 }
