b647e0a02add4e6248e6cd70a00e2c25e6427de8
Sergio Diaz Ruiz <sdiaz-ru@student.42malaga.com>
2022-10-19 11:20:10 +0200

    [Arreglado]:Error al pasar una cadena con '\0' [Solucion]:Eliminar condicion que eliminaba el desbordamineto

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: sdiaz-ru <sdiaz-ru@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 18:41:52 by sdiaz-ru          #+#    #+#             */
-/*   Updated: 2022/10/07 12:24:24 by sdiaz-ru         ###   ########.fr       */
+/*   Updated: 2022/10/19 11:17:26 by sdiaz-ru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_memset(void *str, int c, t_size_t n)
 	t_size_t	i;
 
 	i = 0;
-	while (*(char *)(str + i) && i < n)
+	while (i < n)
 	{
 		*(char *)(str + i) = c;
 		i++;
