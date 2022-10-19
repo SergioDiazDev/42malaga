d627cf463b147a2574d3fac1588a5ea59929bb6b
Sergio Diaz Ruiz <sdiaz-ru@student.42malaga.com>
2022-10-19 11:31:56 +0200

    [Arreglado]:Error al introducir valores nulos. [Solucion]:Añadir guardas para ambos parametros nulos

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: sdiaz-ru <sdiaz-ru@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 08:48:49 by sdiaz-ru          #+#    #+#             */
-/*   Updated: 2022/10/07 12:41:52 by sdiaz-ru         ###   ########.fr       */
+/*   Updated: 2022/10/19 11:30:14 by sdiaz-ru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,12 @@ void	*ft_memcpy(void *dest, const void *src, t_size_t n)
 	s = (const char *)src;
 	d = (char *)dest;
 	i = 0;
+	if (!d && !s)
+		return (NULL);
 	while (i < n)
 	{
 		d[i] = s[i];
 		i++;
 	}
-	return (d);
+	return (dest);
 }
