1d9b380576e577b9943d853016eda081b813df99
sdiaz-ru <sdiaz-ru@studient.42malaga.com>
2023-01-26 12:44:46 +0100

    Get_Next_Line funcional y cumpliendo la norma, Paco arroja errores que aun no he solucionado

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -20,25 +20,23 @@ char	*get_next_line(int fd)
 	int			nl;
 	int			i;
 
+	if (fd <= 0)
+		return (NULL);
 	buff = ft_read(fd, buff);
+	if (!buff)
+		return (NULL);
 	nl = ft_nllen(buff);
 	line = ft_calloc(sizeof(char), nl + 1);
-	if (!line || !buff)
-		return (ft_free(buff, line));
-	ft_memcpy(line, buff, nl);
-	i = 0;
-	while (buff[nl + i])
-	{
+	line = ft_memcpy(line, buff, nl);
+	i = -1;
+	while (buff[nl + ++i])
 		buff[i] = buff[nl + i];
-		i++;
-	}
 	aux = ft_calloc(sizeof(char), i + 1);
-	if (!aux || !buff)
-		return (ft_free(buff, aux));
-	ft_memcpy(aux, buff, i);
+	aux = ft_memcpy(aux, buff, i);
 	free(buff);
 	buff = aux;
-	free(aux);
+	if (!aux || !buff)
+		return (ft_free(buff, aux));
 	return (line);
 }
 
@@ -56,26 +54,24 @@ char	*ft_read(int fd, char *buff)
 		if (!buff)
 		{
 			buff = ft_calloc(sizeof(char), BUFFER_SIZE + 1);
-			if (!buff)
-				return (ft_free(buff, NULL));
 			flag = read(fd, buff, BUFFER_SIZE);
 		}
 		else
 		{
 			aux = ft_calloc(sizeof(char), BUFFER_SIZE + 1);
-			if (!aux)
-				return (ft_free(buff, aux));
 			flag = read(fd, aux, BUFFER_SIZE);
 			buff = ft_strjoin(buff, aux);
 			free(aux);
 		}
+		if (!buff)
+			return (ft_free(buff, aux));
 	}
 	return (buff);
 }
 
 int	ft_nllen(char *buff)
 {
-	int nl;
+	int	nl;
 
 	nl = 0;
 	while (buff[nl])
