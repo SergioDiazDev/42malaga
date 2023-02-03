f751d4e033c606c471e6d8523aa9339f879cbf5e
sdiaz-ru <sdiaz-ru@student.42malaga.com>
2023-02-03 12:08:05 +0100

    Solo queda un leak

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -23,20 +23,21 @@ char	*get_next_line(int fd)
 	if (fd < 0 || BUFFER_SIZE < 1)
 		return (NULL);
 	buff = ft_read(fd, buff);
-	if (ft_strlen(buff) == 0)
-		return (NULL);
 	nl = ft_nllen(buff);
+	if (nl == 0)
+		return (NULL);
 	line = ft_calloc(sizeof(char), nl + 1);
-	line = ft_memcpy(line, buff, nl);
+	ft_memcpy(line, buff, nl);
 	i = -1;
 	while (buff[nl + ++i])
 		buff[i] = buff[nl + i];
 	aux = ft_calloc(sizeof(char), i + 1);
-	aux = ft_memcpy(aux, buff, i);
+	ft_memcpy(aux, buff, i);
 	free(buff);
 	buff = ft_calloc(sizeof(char), i + 1);
-	buff = ft_memcpy(buff, aux, i);
-	return (free(aux), line);
+	ft_memcpy(buff, aux, i);
+	free(aux);
+	return (line);
 }
 
 char	*ft_read(int fd, char *buff)
@@ -45,15 +46,12 @@ char	*ft_read(int fd, char *buff)
 	int		flag;
 
 	flag = 1;
-	
 	while (flag > 0)
 	{
 		if (ft_truenl(buff) == 0)
-		{
 			if (buff[0] != '\0')
 				return (buff);
-		}
-		if (!buff || ft_strlen(buff) == 0)
+		if (!buff)
 		{
 			buff = ft_calloc(sizeof(char), BUFFER_SIZE + 1);
 			flag = read(fd, buff, BUFFER_SIZE);
@@ -63,7 +61,7 @@ char	*ft_read(int fd, char *buff)
 			aux = ft_calloc(sizeof(char), BUFFER_SIZE + 1);
 			flag = read(fd, aux, BUFFER_SIZE);
 			if (flag == 0)
-				return (free(aux), buff);
+				return (buff);
 			buff = ft_strjoin(buff, aux);
 		}
 	}
